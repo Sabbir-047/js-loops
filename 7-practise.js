d3c7152ef92fbd65077d47095d7035ec8d354d84
Sabbir <sabbirbhuiyan026@gmail.com>
2026-01-29 17:21:51 +0600

    half task

---
+++ 7-practise.js
@@ -0,0 +1,17 @@
+// 1 - 60 times
+// ---------------
+for (let i = 1; i <= 60; i++) {
+  // console.log('I will invest 6 hrs here', i);
+}
+
+// 2- Odd Even
+for (let i = 61; i <= 100; i++) {
+  if (i % 2 === 1) {
+    console.log("odd: ", i);
+  }
+}
+for (let i = 61; i <= 100; i++) {
+  if (i % 2 === 0) {
+    console.log("even: ", i);
+  }
+}
